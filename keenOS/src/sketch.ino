0cb0f9bb68672d76b92f1395ad8dc3e2b464ba8d
Bryan Ake <BryanAke@gmail.com>
2013-05-18 02:21:42 -0400

    attempt at starting the whole struct thing. Not going super well.

--- keenOS/src/sketch.ino
+++ keenOS/src/sketch.ino
@@ -9,9 +9,9 @@
 //U8GLIB_DOGXL160_2X_BW u8g(13, 11, 10, 9);	// SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9
 U8GLIB_DOGXL160_2X_GR u8g(13, 11, 10, 9);	// SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9
 
-
 #include "fonts/keen.c"
 #include "fonts/keentitle.c"
+#include "menu.h"
 
 //defines for tracking keypresses
 #define KEY_NONE 0
@@ -35,88 +35,8 @@ const int down_pin = 5;
 const int left_pin = 7;
 const int right_pin = 8;
 
-//Main Menu - options to draw on the main menu.
-char* mainMenu_strings[]={"NEW GAME", "LOAD GAME", "SAVE GAME",
-                  "CONFIGURE", "RETURN TO GAME","END GAME", "PADDLE WAR", "QUIT"};
-int mainMenu_usability[] = {true, true, false, true, false, false, true, true};
-
-int selected = 0;
-int last = 0;
-
 int redraw_required = 0;
 
-void draw() {
-  //u8g_uint_t mx, my;
-  
-  //mx = u8g.getWidth();
-  //mx >>= 1;
-  
-  //my = u8g.getHeight();
-  //my >>= 1;
-  
-  u8g.setColorIndex(2);
-  drawHeader("MAIN MENU");
-  u8g.setColorIndex(1);
-  int i;
-  for (i=0; i< 8; i = i+1) {
-    drawMenuItem(i, mainMenu_strings[i], selected == i, mainMenu_usability[i]);
-  }
-  u8g.setColorIndex(2);
-
-  //u8g.drawHLine(3,84, u8g.getWidth() - 6);
-  u8g.drawHLine(3,85, u8g.getWidth() - 6);
-
-  u8g.setColorIndex(1);
-  
-  u8g.drawStr( 6, 93, "Arrows move      Enter selects");
-  u8g.drawStr( 51, 100, "ESC to quit");
-  u8g.setColorIndex(2);
-}
-
-/*
- * drawHeader(title)
- * ----
- * will draw a header for a menu or info page on the top of the screen using the title font
- * with a line continuing around it.
- */
-void drawHeader(char title[]) {
-  //draw lines to left of title
-  //u8g.drawHLine(3,5, 7);
-  u8g.drawHLine(3,6, 7);
-
-  u8g.setFont(keentitle);
-  u8g.drawStr( 11, 10, title);
-
-  u8g_uint_t title_width = u8g.getStrWidth(title);
-
-  u8g_uint_t line_x = title_width + 11;
-  
-  //u8g.drawHLine(line_x ,5, u8g.getWidth() - (line_x + 3) );
-  u8g.drawHLine(line_x ,6, u8g.getWidth() - (line_x + 3) );
-
-  //reset the font to the one used for the menu
-  u8g.setFont(keen);
-
-}
-
-void drawMenuItem(int index, char text[], boolean highlight, boolean usable) {
-  //draw the menu option with the given parameters..
-  char* icon = "+";
-  if (!usable) {
-    icon = "-";
-  }
-
-  if (highlight) {
-    //up the brightness..
-    u8g.setColorIndex(2);
-    u8g.drawStr(41, 20 + index*8, icon);
-    u8g.drawStr(46, 20 + index*8, text);
-    u8g.setColorIndex(1);      
-  } else {
-    u8g.drawStr(41, 20 + index*8, icon);
-    u8g.drawStr(46, 20 + index*8, text);
-  }
-}
 
 
 void setup(void) {
@@ -184,7 +104,7 @@ void loop(void) {
     u8g.firstPage();  
     do {
       
-      draw();
+      drawMenu(mainMenu);
     } while( u8g.nextPage() );
     redraw_required = 0;
   }
