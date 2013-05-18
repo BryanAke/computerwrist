a7bc40c1708e0d8394d1f44621845988ce82464c
Bryan Ake <BryanAke@gmail.com>
2013-05-18 00:03:19 -0400

    added input pins. Hooked up to dev input board for testing purposes, with external pull-up resistors. test print value in draw function to show value of pin5.

--- keenOS/src/sketch.ino
+++ keenOS/src/sketch.ino
@@ -33,7 +33,7 @@ void drawHeader(char title[]) {
 
 }
 
-void draw(void) {
+void draw(int pinStatus) {
   //u8g_uint_t mx, my;
   
   //mx = u8g.getWidth();
@@ -49,31 +49,57 @@ void draw(void) {
   u8g.drawStr( 41, 36, "-SAVE GAME");
   u8g.drawStr( 41, 44, "+CONFIGURE");
   u8g.drawStr( 41, 52, "-RETURN TO GAME");
-  u8g.drawStr( 41, 60, "+END GAME");
+  u8g.drawStr( 41, 60, "-END GAME");
   u8g.drawStr( 41, 68, "+PADDLE WAR");
   u8g.drawStr( 41, 76, "+QUIT");
 
+  if (pinStatus == LOW) {
+    u8g.drawStr( 15, 15, "LOW");
+
+  } else {
+
+    u8g.drawStr( 15, 15, "HIGH");
+
+  }
 
   u8g.drawHLine(3,84, u8g.getWidth() - 6);
   u8g.drawHLine(3,85, u8g.getWidth() - 6);
 
-
+  
   u8g.drawStr( 6, 93, "Arrows move      Enter selects");
   u8g.drawStr( 51, 100, "ESC to quit");
 }
+//set up pins for buttons.
+//up/down -- flat buttons in middle of left side
+const int up_pin = 5;
+const int down_pin = 6;
+
+//left/right -- round buttons on bottom-left corner.
+const int left_pin = 7;
+const int right_pin = 8;
 
 void setup(void) {
+  //setup input pins
+
+  //four buttons
+  pinMode(up_pin, INPUT);
+  pinMode(down_pin, INPUT);
+  pinMode(left_pin, INPUT);
+  pinMode(right_pin, INPUT);
+
+  //set font to default font
   u8g.setFont(keen);
 }
 
 void loop(void) {
-  
+  int pinStatus = digitalRead(5);
   // picture loop
   u8g.firstPage();  
   do {
-    draw();
+    
+    draw(pinStatus);
   } while( u8g.nextPage() );
   
   // rebuild the picture after some delay
-  delay(100);
+  //delay(100);
 }
